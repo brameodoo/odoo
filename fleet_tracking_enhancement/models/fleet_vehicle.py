0924ba190dd4f254836bc6af5c1b1aac0c5ea65b
brameodoo <brame.odoo@gmail.com>
2025-05-19 11:33:52 -0600

    Update fleet_vehicle.py

--- fleet_tracking_enhancement/models/fleet_vehicle.py
+++ fleet_tracking_enhancement/models/fleet_vehicle.py
@@ -10,6 +10,11 @@ class FleetVehicle(models.Model):
     x_studio_distrito = fields.Char(string='x_studio_distrito', tracking=True)
     category_id = fields.Many2one(string='category_id', tracking=True)
     mobility_card = fields.Char(string='mobility_card', tracking=True)
+    next_assignation_date = fields.Date(string='next_assignation_date', tracking=True)
+    order_date = fields.Date(string='order_date', tracking=True)
+    acquisition_date = fields.Date(string= 'acquisition_date', tracking=True)
+    odometer = fields.Float(string='odometer', tracking=True)
+    manager_id = fields.Many2one(string='manager_id', tracking=True)
     # Añade aquí otros campos que quieras rastrear
     # Ejemplo:
     # manager_id = fields.Many2one('res.users', string='Manager', tracking=True)
